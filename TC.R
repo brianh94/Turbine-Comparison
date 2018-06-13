0e71ab7fa2768b7f29854dc3c20353b6ff53e09d
brianh94 <weeebrian@gmail.com>
2018-06-13 10:10:08 -0700

    blah

---
+++ TC.R
@@ -0,0 +1,11 @@
+library('rio')
+library('plotly')
+tg_comparison<-import("G:/Software-WPRS/2014-16 Comparative Analysis/2016_TG_comparison.csv")
+
+# w/ labels
+tg<- plot_ly(tg_comparison, x = ~RotorArea_m2, y = ~Capacity_kW, z = ~GroupCF,size = ~TurbineCount, sizes=c(1,500), color = ~WRA, colors = c('#f44242','#f48041','#f4f441','#67f441','#41f1f4','#416df4','#8541f4','#f441be'),
+             text=~paste('Project:',Project,'<br>Turbine Model:',Manufacturer,Model,'<br>Turbine Capacity:',Capacity_kW,'kW<br>Turbine Count:',TurbineCount,'<br>Rotor Area:',RotorArea_m2,'m^2<br>Group Capacity:',GroupCapacity_kW,'kW<br>Group Capacity Factor:',GroupCF)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'Rotor Area (m^2)'),
+                      yaxis = list(title = 'Turbine Capacity (kW)'),
+                      zaxis = list(title = 'Group CF')))
